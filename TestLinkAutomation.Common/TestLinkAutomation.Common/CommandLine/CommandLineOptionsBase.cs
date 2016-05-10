3fabeebbe5eeb04102d439a59eeb7ed112d64b64
Domenik Melcher <m.domenik@gmail.com>
2016-05-10 12:48:36 +0200

    Added Project TestLinkAutomation.Common with the abstract module logic ModulLogicBase

---
+++ TestLinkAutomation.Common/TestLinkAutomation.Common/CommandLine/CommandLineOptionsBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Plossum.CommandLine;
+
+namespace TestLinkAutomation.Common.CommandLine
+{
+    [CommandLineManager()]
+    public class CommandLineOptionsBase
+    {
+        [CommandLineOption(Description = "Displays this help text")]
+        public bool Help = false;
+
+        [CommandLineOption(Description = "Specifies the output file path")]
+        public string output_path
+        {
+            get { return output_string; }
+            set
+            {
+                output_string = value;
+            }
+        }
+
+        private string output_string;        
+    }
+}
---
+++ TestLinkAutomation.Common/TestLinkAutomation.Common/ModulLogicBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLinkAutomation.Common.CommandLine;
+using Plossum.CommandLine;
+
+namespace TestLinkAutomation.Common
+{
+    public abstract class ModulLogicBase<T> where T : CommandLineOptionsBase
+    {
+        protected T CommandLineOptions { get; set; }
+
+        public virtual void InitializeModul()
+        {
+            InitializeCommandLineOptions();
+        }
+
+        public abstract void ExecuteModulLogic();        
+
+        public abstract void Save();
+
+        protected abstract void ExecutePreProcessing();     
+
+        private void InitializeCommandLineOptions()
+        {
+            CommandLineParser parser = new CommandLineParser(CommandLineOptions);
+            parser.Parse();
+
+            if (CommandLineOptions.Help)
+            {
+                Console.WriteLine(parser.UsageInfo.GetOptionsAsString(90));
+                Environment.Exit(0);
+            }
+            else
+                if (parser.HasErrors)
+                {
+                    Console.WriteLine(parser.UsageInfo.GetErrorsAsString(90));
+                    Environment.Exit(-1);
+                }
+        }
+    }
+}
